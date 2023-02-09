27c1aefcddad3652b03a3f0bab30d8be87036d0f
Artur Siedlerski <don.siedler@gmail.com>
2023-02-09 22:24:54 +0100

    Add Bearer authorisation to Sheety requests

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import requests
-from local_settings import APP_ID, API_KEY
+from local_settings import APP_ID, API_KEY, AUTH_TOKEN
 from local_settings import GENDER, WEIGHT_KG, HEIGHT_CM, AGE, USERNAME
 
 PROJECT_NAME = "workoutTracking"
@@ -8,10 +8,14 @@ SHEET_NAME = "workouts"
 NUTRITIONIX_API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 SHEETY_API_ENDPOINT = f"https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
 
-headers = {
+NUTRITIONIX_API_HEADERS = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
     "Content-Type": "application/json",
+}
+
+SHEETY_API_HEADERS = {
+    "Authorization": f"Bearer {AUTH_TOKEN}",
 }
 
 workout_input = input("Tell me what did you do today?\n")
@@ -24,7 +28,7 @@ api_params = {
     "age": AGE,
 }
 
-response = requests.post(NUTRITIONIX_API_ENDPOINT, headers=headers, json=api_params)
+response = requests.post(NUTRITIONIX_API_ENDPOINT, headers=NUTRITIONIX_API_HEADERS, json=api_params)
 response.raise_for_status()
 workout_data = response.json()["exercises"]
 
@@ -40,6 +44,6 @@ for exercise in workout_data:
             "calories": exercise["nf_calories"],
         }
     }
-    r = requests.post(SHEETY_API_ENDPOINT, json=row_to_save)
+    r = requests.post(SHEETY_API_ENDPOINT, headers=SHEETY_API_HEADERS, json=row_to_save)
     r.raise_for_status()
     print(r.json())
