f271d20cd2270b9655f54355dd593b62e77db5ea
Artur Siedlerski <don.siedler@gmail.com>
2023-02-09 22:12:06 +0100

    Fix data formatting; save data to rows in Google Sheets

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import requests
 from local_settings import APP_ID, API_KEY
 from local_settings import GENDER, WEIGHT_KG, HEIGHT_CM, AGE, USERNAME
 
-API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+PROJECT_NAME = "workoutTracking"
+SHEET_NAME = "workouts"
+NUTRITIONIX_API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEETY_API_ENDPOINT = f"https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
 
 headers = {
     "x-app-id": APP_ID,
@@ -21,22 +24,22 @@ api_params = {
     "age": AGE,
 }
 
-response = requests.post(API_ENDPOINT, headers=headers, json=api_params)
+response = requests.post(NUTRITIONIX_API_ENDPOINT, headers=headers, json=api_params)
+response.raise_for_status()
 workout_data = response.json()["exercises"]
-
-PROJECT_NAME = "workoutTracking"
-SHEET_NAME = "workouts"
-SHEETY_API_ENDPOINT = f"https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
 
 now = datetime.now()
 
 for exercise in workout_data:
     row_to_save = {
-        "date": now.strftime("%d/%m/%Y"),
-        "time": now.strftime("%X"),
-        "exercise": exercise["name"],
-        "duration": exercise["duration_min"],
-        "calories": exercise["nf_calories"],
+        "workout": {
+            "date": now.strftime("%d/%m/%Y"),
+            "time": now.strftime("%X"),
+            "exercise": exercise["name"],
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
     }
-    print(row_to_save)
-
+    r = requests.post(SHEETY_API_ENDPOINT, json=row_to_save)
+    r.raise_for_status()
+    print(r.json())
