d992fcfee0578297d922f431f9cef028b5775e98
Artur Siedlerski <don.siedler@gmail.com>
2023-02-09 22:06:37 +0100

    Add Sheety API; format data returning from Nutritionix API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 import requests
 from local_settings import APP_ID, API_KEY
-from local_settings import GENDER, WEIGHT_KG, HEIGHT_CM, AGE
+from local_settings import GENDER, WEIGHT_KG, HEIGHT_CM, AGE, USERNAME
 
 API_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -20,6 +21,22 @@ api_params = {
     "age": AGE,
 }
 
-
 response = requests.post(API_ENDPOINT, headers=headers, json=api_params)
-print(response.text)
+workout_data = response.json()["exercises"]
+
+PROJECT_NAME = "workoutTracking"
+SHEET_NAME = "workouts"
+SHEETY_API_ENDPOINT = f"https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
+
+now = datetime.now()
+
+for exercise in workout_data:
+    row_to_save = {
+        "date": now.strftime("%d/%m/%Y"),
+        "time": now.strftime("%X"),
+        "exercise": exercise["name"],
+        "duration": exercise["duration_min"],
+        "calories": exercise["nf_calories"],
+    }
+    print(row_to_save)
+
